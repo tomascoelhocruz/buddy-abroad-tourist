7b95af3271223495e176be24443201eea08cacbe
unknown <tomascoelhocruz@gmail.com>
2021-10-06 13:19:38 +0100

    home page section base layer

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,17 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    SwiperModule,
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,58 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  mockData: any = {
+    "nearbyLocations": [
+      {
+        "name": "Lisboa",
+        "image": "placeholder",
+        "experiencesAvailable": 45,
+      },
+      {
+        "name": "Sintra",
+        "image": "placeholder",
+        "experiencesAvailable": 21,
+      },
+      {
+        "name": "Setubal",
+        "image": "placeholder",
+        "experiencesAvailable": 7,
+      },
+      {
+        "name": "Ericeira",
+        "image": "placeholder",
+        "experiencesAvailable": 13,
+      },
+    ],
+    "suggestedExperiences": [
+      {
+        "name": "Pastéis de Belém",
+        "image": "placeholder",
+        "price": 15,
+        "rating": 5,
+      },
+      {
+        "name": "Sunset Tour",
+        "image": "placeholder",
+        "price": 36,
+        "rating": 5,
+      },
+      {
+        "name": "Tuk Tuk Ride",
+        "image": "placeholder",
+        "price": 9,
+        "rating": 3.6,
+      },
+      {
+        "name": "Pub Crawl",
+        "image": "placeholder",
+        "price": 22,
+        "rating": 4.5,
+      },
+    ]
+  }
+
+  constructor() { }
+
 
 }
