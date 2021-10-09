28594e5d70d8e3e3f7fd98cf26d4a6ad93b00c8a
unknown <tomascoelhocruz@gmail.com>
2021-10-09 18:08:49 +0100

    search tour, search buddy pages

--- src/app/search-buddy/search-buddy.page.ts
+++ src/app/search-buddy/search-buddy.page.ts
@@ -7,7 +7,61 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./search-buddy.page.scss'],
 })
 export class SearchBuddyPage implements OnInit {
-  
+
+
+  mockData: any = {
+    "buddies": [
+      {
+        "name": "Alex Jones",
+        "image": "assets/avatar-finn.png",
+        "description": "hey there",
+        "interests": [
+          "Museums", "Party", "Outside"
+        ]
+      },
+      {
+        "name": "Alex Jones",
+        "image": "assets/avatar-finn.png",
+        "description": "hey there",
+        "interests": [
+          "Museums", "Party"
+        ]
+      },
+      {
+        "name": "Alex Jones",
+        "image": "assets/avatar-finn.png",
+        "description": "hey there",
+        "interests": [
+          "Museums", "Party", "Outside"
+        ]
+      },
+      {
+        "name": "Alex Jones",
+        "image": "assets/avatar-finn.png",
+        "description": "hey there",
+        "interests": [
+          "Party", "Outside"
+        ]
+      },
+      {
+        "name": "Alex Jones",
+        "image": "assets/avatar-finn.png",
+        "description": "hey there",
+        "interests": [
+          "Party"
+        ]
+      },
+      {
+        "name": "Alex Jones",
+        "image": "assets/avatar-finn.png",
+        "description": "hey there",
+        "interests": [
+          "Museums"
+        ]
+      },
+    ],
+  }
+
   constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
--- src/app/search-tours/search-tours.page.ts
+++ src/app/search-tours/search-tours.page.ts
@@ -10,7 +10,78 @@ export class SearchToursPage implements OnInit {
 
   constructor(private navCtrl: NavController) { }
 
+  mockData: any = {
+    "experiences": [
+      {
+        "name": "Pastéis de Belém",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 15,
+        "rating": 5,
+      },
+      {
+        "name": "Sunset Tour",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 36,
+        "rating": 5,
+      },
+      {
+        "name": "Tuk Tuk Ride",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 9,
+        "rating": 3.6,
+      },
+      {
+        "name": "Pub Crawl",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 22,
+        "rating": 4.5,
+      },
+      {
+        "name": "Live music",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 5,
+        "rating": 3.5,
+      },
+      {
+        "name": "Food tour",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 40,
+        "rating": 5,
+      },
+        {
+        "name": "Live music",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 5,
+        "rating": 3.5,
+      },
+      {
+        "name": "Food tour",
+        "image": "ruas-mais-bonitas-de-lisboa-1.jpg",
+        "description": "lorem impsum dolor et nominum papis supremo et aqua",
+        "price": 40,
+        "rating": 5,
+      },
+    ]
+  }
+
+  experiences: any
+
+  // helper function to change simple array to x dimensional array
+  arrayToMatrix = (array, columns) => Array(Math.ceil(array.length / columns)).fill('').reduce((acc, cur, index) => {
+    return [...acc, [...array].splice(index * columns, columns)]
+  }, [])
+
+
   ngOnInit() {
+    this.experiences = this.arrayToMatrix(this.mockData.experiences, 2)
+
   }
 
   goback() {
